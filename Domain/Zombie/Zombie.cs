18c3731cb6ff7aca7a2754ca957ad8862180c0d9
Landry <landrycharlier@hotmail.com>
2017-10-29 14:33:57 +0100

    zombie data in Domain

---
+++ Domain/Zombie/Zombie.cs
@@ -0,0 +1,14 @@
+﻿using Core;
+
+namespace Domain.Zombie
+{
+    public abstract class Zombie
+    {
+        public int Action { get; protected set; }
+        public int Experience { get; protected set; }
+        public int MinDamage { get; protected set; }
+        public int Priority { get; protected set; }
+        public string Name { get; protected set; }
+        public int ZonePerMove { get; protected set; }
+    }
+}
