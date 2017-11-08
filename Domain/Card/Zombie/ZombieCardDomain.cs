fb6b5981994ded8f6189fa27eee5b20d5bed0aec
Landry <landrycharlier@hotmail.com>
2017-11-08 22:32:35 +0100

    zombie card data in domain

---
+++ Domain/Card/Zombie/ZombieCardDomain.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Domain.Card.Zombie
+{
+    public class ZombieCardDomain
+    {
+        public InvasionDomain BlueInvasion { get; set; }
+        public InvasionDomain YellowInvasion { get; set; }
+        public InvasionDomain OrangeInvasion { get; set; }
+        public InvasionDomain RedInvasion { get; set; }
+        public bool IsSewerInvasion { get; set; }
+        public bool IsBlueBedInvasion { get; set; }
+        public bool IsGreenBedInvasion { get; set; }
+        public bool IsSupplementaryActivation { get; set; }
+    }
+
+    public class InvasionDomain
+    {
+        public int ZombieNumber { get; set; }
+        public string ZombieType { get; set; }
+    }
+}
